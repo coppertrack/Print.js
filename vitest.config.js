812e68302c4177d106b0ac6e00fecf4ae33cacfb
ju5t <m@ju5t.nl>
2025-10-11 10:03:56 +0200

    refactor: vitest coverage and github actions

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'jsdom',
-    globals: true
+    globals: true,
+    coverage: {
+      reporter: ['text', 'lcov', 'html'],
+      include: ['src/**/*.ts'],
+      exclude: ['src/types.ts']
+    }
   }
 })
